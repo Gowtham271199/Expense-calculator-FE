14573e8c929afe5a40cd5748d39e833ce3b05086
Gowtham271199 <gothammelo27@gmail.com>
2024-08-18 16:12:24 +0530

    register url

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('https://expense-calculator-be-2.onrender.com/api/auth/register', { name, email, password });
       console.log(response.data);
-      window.location.href = '/login';
+      navigate('/login'); // Use useNavigate hook for redirection
     } catch (error) {
-      console.error(error.response.data);
+      console.error(error.response?.data || error.message); // Handle undefined error.response
     }
   };
 
