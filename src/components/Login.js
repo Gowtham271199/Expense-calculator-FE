569ea0c84198d8f4856428e132cff5f1be8184bd
Gowtham271199 <gothammelo27@gmail.com>
2024-08-18 16:21:15 +0530

    login navigate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try{
     const res = await axios.post('https://expense-calculator-be-2.onrender.com/api/auth/login', { email, password });
     localStorage.setItem('token', res.data.token);
-    window.location.href = '/expenses';
+    navigate('/expenses');
+    }catch (error) {
+      console.error(error.response?.data || error.message); // Handle undefined error.response
+    }
   };
 
   return (
